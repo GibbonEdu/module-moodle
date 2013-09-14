372f7a3fcb19146d447014774b2f8404eda89d65
Ross Parler <rparker@MBP012-Ross-Parker.local>
2013-09-15 02:33:28 +0800

    Update From Local

---
+++ CHANGEDB.php
@@ -0,0 +1,11 @@
+<?
+//USE ;end TO SEPERATE SQL STATEMENTS. DON'T USE ;end IN ANY OTHER PLACES!
+
+$sql=array() ;
+$count=0 ;
+
+//v1.0.00 - First version so no code here
+$sql[$count][0]="1.0.00" ;
+$sql[$count][1]="" ;
+?>
+
